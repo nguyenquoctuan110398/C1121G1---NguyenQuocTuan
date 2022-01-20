59dc48aa11a213208185bb5054a8f5225a7606b0
nguyenquoctuan110398 <nguyenquoctuan11031998@gmail.com>
2022-01-20 17:29:25 +0700

    update

--- module 2/src/ss15_xu_li_ngoai_le/bai_tap/IllegalTriangleExceptionUsing/Main.java
+++ module 2/src/ss15_xu_li_ngoai_le/bai_tap/IllegalTriangleExceptionUsing/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         boolean flag = true;
-        int[] arr = new int[3];
+        double[] arr = new double[3];
         while (flag) {
             flag = false;
             try {
                 arr = inputTriangleEdge();
             } catch (IllegalTriangleException e) {
-                e.getStackTrace();
+                e.printStackTrace();
                 flag = true;
                 System.out.println("Re-enter the value: ");
             } catch (Exception e) {
@@ -20,28 +20,29 @@ public class Main {
             }
         }
 
-        String str="";
+        String str = "";
 
-        for (int i = 0; i < arr.length; i++){
-            str+=arr[i] + " ";
+        for (int i = 0; i < arr.length; i++) {
+            str += arr[i] + " ";
         }
         str += "là 3 cạnh của tam giác";
         System.out.println(str);
     }
 
-    public static int[] inputTriangleEdge() throws IllegalTriangleException {
+    public static double[] inputTriangleEdge() throws IllegalTriangleException {
         Scanner scanner = new Scanner(System.in);
-        int[] arrTriangleEdge = new int[3];
+        double[] arrTriangleEdge = new double[3];
         for (int i = 0; i < arrTriangleEdge.length; i++) {
             System.out.print("Enter element " + (i + 1) + ": ");
             arrTriangleEdge[i] = Integer.parseInt(scanner.nextLine());
         }
 
-        if (arrTriangleEdge[0] <= 0 || arrTriangleEdge[1] <= 0 || arrTriangleEdge[1] <= 0 ||
-                (arrTriangleEdge[0] + arrTriangleEdge[1]) <= arrTriangleEdge[2] ||
+        if (arrTriangleEdge[0] <= 0 || arrTriangleEdge[1] <= 0 || arrTriangleEdge[2] <= 0) {
+            throw new IllegalTriangleException("Side length of triangle is greater than 0");
+        } else if ((arrTriangleEdge[0] + arrTriangleEdge[1]) <= arrTriangleEdge[2] ||
                 (arrTriangleEdge[1] + arrTriangleEdge[2]) <= arrTriangleEdge[0] ||
                 (arrTriangleEdge[0] + arrTriangleEdge[2]) <= arrTriangleEdge[1]) {
-            throw new IllegalTriangleException("Not 3 sides of a triangle");
+            throw new IllegalTriangleException("The sum of the lengths of the 2 sides must be greater than the other side");
         }
         return arrTriangleEdge;
     }
